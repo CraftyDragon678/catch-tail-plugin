c27b6c2d589f765db9eb0a661af8e1b843bea688
CraftyDragon678 <craftydragon678@gmail.com>
2020-01-18 18:39:32 +0900

    add EventManager class

---
+++ src/main/kotlin/com/cragon/catchtail/EventManager.kt
@@ -0,0 +1,12 @@
+package com.cragon.catchtail
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerMoveEvent
+
+class EventManager : Listener {
+    @EventHandler
+    fun onPlayerMove(e: PlayerMoveEvent) {
+
+    }
+}
